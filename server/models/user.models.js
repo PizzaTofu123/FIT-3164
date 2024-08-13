d834b251028793d5d1d501cfdbf630ce6bad30ab
PizzaTofu123 <140805748+PizzaTofu123@users.noreply.github.com>
2024-08-13 13:42:43 +1000

    print error when not connecting

--- server/models/user.models.js
+++ server/models/user.models.js
@@ -3,10 +3,6 @@ const mongoose = require('mongoose');
 const {isAlphanumeric, isNumeric} = require('../utility/stringValidator')
 
 const UserSchema = mongoose.Schema({
-    _id: {
-        type: String,
-        required: true,
-    },
     firstName: {
         type: String,
         required: [true, "Please enter first name"],
@@ -55,11 +51,7 @@ const UserSchema = mongoose.Schema({
     {
         // 2 extra fields for created and updated at
         Timestamps: true
-    },
-    {
-        _id: false  // This disables automatic generation of the _id field
     }
-
 );
 
 //for mongodb to use
