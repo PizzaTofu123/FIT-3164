f19a181c3a9eb65a968cf69c8d0927f2fa7d756a
joannamjy <111515513+joannamjy@users.noreply.github.com>
2024-08-19 20:05:56 +1000

    Test Navigation Bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,33 @@
-import React, {useEffect, useState} from 'react'
-
+import React, { useEffect, useState } from 'react';
+import NavBar from './components/NavBar'; // Adjust the path if necessary
 
 function App() {
-  const [backendData, setBackendData] = useState([{}])
- //getting data from backend and setting it into the backendData variable
-  useEffect (() => {
-  fetch('/api').then(
-    response => response.json()
-  ).then(
-    data => {setBackendData(data)}
-  )
- }, []) // only runs in the first render of the component
+  const [backendData, setBackendData] = useState([{}]);
+
+  // Getting data from backend and setting it into the backendData variable
+  useEffect(() => {
+    fetch('/api')
+      .then(response => response.json())
+      .then(data => {
+        setBackendData(data);
+      });
+  }, []); // only runs in the first render of the component
+
   return (
     <div>
-    
-    {
-      /*
-    (typeof backendData.user == "undefined") ? (
-      <p>Loading</p>
-    ): (
-      backendData.user.map((u, i) => (<p key = {i}>{u}</p>))
-    )*/
+      {/* Render the NavBar at the top of the page */}
+      <NavBar />
 
-    backendData.map((dataObj, index) => {
+      {/* Render the backend data below the NavBar */}
+      {backendData.map((dataObj, index) => {
         return (
-          <p style={{ fontSize: 20, color: 'black' }}>{dataObj.firstName}</p>
+          <p key={index} style={{ fontSize: 20, color: 'black' }}>
+            {dataObj.firstName}
+          </p>
         );
-      })
-    }
+      })}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
