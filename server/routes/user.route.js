ad4b70ebf05595f86880ad80c300e5da72b08684
ntioschool <111689779+ntioschool@users.noreply.github.com>
2024-10-28 16:57:09 +1100

    updated user comments

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -4,18 +4,23 @@ const User = require("../models/user.models");
 const router = express.Router();
 const UserController = require("../controller/user.controller");
 
+// create new user
 router.post('/', UserController.createOneUser);
 
+// return all users
 router.get('/', UserController.getAllUser);
+
+// get user by id
 router.get('/:userId', UserController.getOneUser);
 
+// get user specific forget password question
 router.get('/forgetPassword', UserController.getForgetQue);
 router.post('/forgetPassword', UserController.forgetPassword);
 
+// update user by user id
 router.put('/:userId', UserController.updateOneUser);
 
-// router.put('/:userId/setRecoveryCode', UserController.setRecoveryCode)
-
+// delete user by user id
 router.delete('/:userId', UserController.deleteOneUser);
 
 // check-email route
@@ -24,6 +29,7 @@ router.post('/check-email', UserController.checkEmailExists);
 router.post('/login', UserController.login);
 
 router.post('/populate', UserController.populateUser);
+
 router.post('/populateClub', UserController.populateClub);
 
 module.exports = router;
