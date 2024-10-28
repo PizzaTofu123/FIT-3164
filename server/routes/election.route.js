691d44a33b6d4fc891cda5eb6be55eb75c200a09
ntioschool <111689779+ntioschool@users.noreply.github.com>
2024-10-28 16:56:46 +1100

    comments for election

--- server/routes/election.route.js
+++ server/routes/election.route.js
@@ -5,13 +5,19 @@ const router = express.Router();
 const ElectionController = require("../controller/election.controller");
 
 
-
+// create new election
 router.post('/', ElectionController.createElection);
 
+// return all elections
 router.get('/', ElectionController.getAllElection);
+
+// get election by election id
 router.get('/:electionId', ElectionController.getOneElection);
 
+// update election by election id
 router.put('/:electionId', ElectionController.updateElection);
+
+// delete election by election id
 router.delete('/:electionId', ElectionController.deleteOneElection);
 
 module.exports = router;
